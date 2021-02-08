d29de15ce77a480b01763fe1d7d028f2d6581c90
Michael Towson <MTowson@gmail.com>
2021-02-08 12:55:59 -0800

    first commit

---
+++ Pythagorean_Theorem.java
@@ -0,0 +1,7 @@
+public class Pythagorean_Theorem {
+    public static void main(String[] args){
+        Pythagorean pyCalculation = new Pythagorean();
+        double result = pyCalculation.calculateHypotenuse(3, 5);
+        System.out.println(result);
+    }
+}
